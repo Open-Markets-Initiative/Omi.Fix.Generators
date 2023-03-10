dea62d67e843e39b0ff40f250c65066fb3007c04
Omi Dev <omi.development@protonmail.com>
2023-03-10 09:02:56 -0600

    Refactor proj files

--- Generate.Example.Fixml/Program.cs
+++ Generate.Example.Fixml/Program.cs
@@ -1,7 +1,9 @@
-﻿// Load standard fixml from library
+﻿// Load fixml from library
 var fixml = Omi.Fixml.Document.From(@"Library\Fixml\Fix.v4.2.xml");
 
 // Convert to normalized specification
 var specification = fixml.ToSpecification();
+
+// make a few edits
 
 
