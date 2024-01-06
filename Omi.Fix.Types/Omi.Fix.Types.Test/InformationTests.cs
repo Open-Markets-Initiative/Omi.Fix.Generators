dea2a886e4a2334022e3689eddda2d4e8974d23d
Myles Kaufman <myleskaufman.mk@gmail.com>
2024-01-05 19:15:48 -0500

    Added fix types xml tests

---
+++ Omi.Fix.Types/Omi.Fix.Types.Test/InformationTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Omi.Fix.Types.Test{
+    using NUnit.Framework;
+    public class InformationTests
+    {
+        [Test]
+        public void VerifyInformationMajor() {
+            var actual = Library.Fix42.Information.Major;
+            var expected = string.Empty;
+
+            Assert.That(actual, Is.EqualTo(expected), "Verify fix major version");  
+        }
+
+        [Test] 
+        public void VerifyInformationMinor() {
+            var actual = Library.Fix42.Information.Minor;
+            var expected = string.Empty;
+
+            Assert.That(actual, Is.EqualTo(expected), "Verify fix minor version");
+        }
+
+        [Test]
+        public void VerifyInformationSource() {
+            var actual = Library.Fix42.Information.Source;
+            var expected = "Library\\Fields\\Fix42.Fields.xml";
+
+            Assert.That(actual, Is.EqualTo(expected), "Verify fix source");
+        }
+    }
+}
---
+++ Omi.Fix.Types/Omi.Fix.Types.Test/TypeTests.cs
@@ -0,0 +1,61 @@
+﻿namespace Omi.Fix.Types.Test{
+    using NUnit.Framework;
+
+    public class TypeTests {
+
+        Type Account;
+
+        [OneTimeSetUp] 
+        public void SetUp() {
+            Account = Library.Fix42.Fields["Account"];
+        }
+
+        [Test]
+        public void VerifyDataTypeFromTypes() {
+            var actual = Account.DataType;
+            var expected = "String";
+
+            Assert.That(actual, Is.EqualTo(expected), "Incorrect DataType from Types");
+        }
+
+        [Test]
+        public void VerifyVersionFromTypes() {
+            var actual = Account.Version;
+            var expected = "FIX42";
+
+            Assert.That(actual, Is.EqualTo(expected), "Incorrect Version from Types");
+        }
+
+        [Test]
+        public void VerifyNameFromTypes() {
+            var actual = Account.Name;
+            var expected = "Account";
+
+            Assert.That(actual, Is.EqualTo(expected), "Incorrect Name from Types");
+        }
+
+        [Test]
+        public void VerifyTagFromTypes() {
+            var actual = Account.Tag;
+            var expected = 1;
+
+            Assert.That(actual, Is.EqualTo(expected), "Incorrect Tag from Types");
+        }
+
+        [Test]
+        public void VerifyIsEnumFromTypes() {
+            var actual = Account.IsEnum;
+            var expected = false;
+
+            Assert.That(actual, Is.EqualTo(expected), "Incorrect IsEnum from Types");
+        }
+
+        [Test]
+        public void VerifyDescriptionFromTypes() {
+            var actual = Account.Description;
+            var expected = "Account mnemonic as agreed between broker and institution.";
+
+            Assert.That(actual, Is.EqualTo(expected), "Verify FIX Types XML element description");
+        }
+    }
+}
