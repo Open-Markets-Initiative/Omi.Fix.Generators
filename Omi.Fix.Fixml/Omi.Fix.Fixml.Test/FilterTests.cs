d5580eb57d6113cf7b6bc2a46c5638c7534263c0
Myles Kaufman <myleskaufman.mk@gmail.com>
2024-02-02 11:58:58 -0600

    Reworked the Document filter method to correctly normalize components and fields.

---
+++ Omi.Fix.Fixml/Omi.Fix.Fixml.Test/FilterTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Omi.Fix.Fixml.Test {
+    using NUnit.Framework;
+    public class FilterTests {
+
+        [Test]
+        public void VerifyFilterComponents() {
+            var document = Omi.Fixml.Library.Fix50;
+            var components = document.Filter(message => message.Name.Equals("ExecutionReport"));
+
+            var expected = 36;
+            var actual = document.Components.Count;
+
+            Assert.That(actual, Is.EqualTo(expected), "Document filter incorrectly normalized components");
+        }
+
+        [Test]
+        public void VerifyFilterFields()
+        {
+            var document = Omi.Fixml.Library.Fix50;
+            var components = document.Filter(message => message.Name.Equals("ExecutionAcknowledgement"));
+
+            var expected = 250;
+            var actual = document.Fields.Count;
+
+            Assert.That(actual, Is.EqualTo(expected), "Document filter incorrectly normalized fields");
+        }
+    }
+}
