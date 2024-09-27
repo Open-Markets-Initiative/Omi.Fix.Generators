8209ac35c6c1dbe5a49b67869b85bcdf061c8bce
Open-Markets-Initiative <contact.omi@protonmail.com>
2024-09-27 14:43:39 -0500

    Update fixml load and readme

--- Omi.Fix.Fixml/Omi.Fix.Fixml/ReadMe.md
+++ Omi.Fix.Fixml/Omi.Fix.Fixml/ReadMe.md
@@ -1,5 +1,9 @@
 ﻿# Omi Fixml Format
 
-Programmatically read, write, and edit Fixml xml formats.  
+FIXML is the XML representation and vocabulary for FIX application messages.
+
+Programmatically read, write, edit and manipulate FIXML.
+
+https://www.fixtrading.org/standards/fixml-online
 
 Also can convert to and from Omi Normalized Fix Specification.
--- Omi.Fix.Fixml/Omi.Fix.Fixml/Xml/Load.cs
+++ Omi.Fix.Fixml/Omi.Fix.Fixml/Xml/Load.cs
@@ -1,15 +1,61 @@
 ﻿namespace Omi.Fixml;
+    using System.IO;
+    using System.Xml;
+    using System.Xml.Serialization;
 
 /// <summary>
-///  Load fixml elements into generated object classes
+///  Load xml elements into generated object classes
 /// </summary>
 
 public static class Load
 {
+    /// <summary>
+    ///  Load classes from file path
+    /// </summary>
+    public static T From<T>(string xml)
+    {
+        using var reader = XmlReader.Create(xml);
+
+        var serializer = XmlSerializer.FromTypes([typeof(T)])[0];
+        if (serializer == null)
+        {
+            throw new Exception();
+        }
+
+        var classes = serializer.Deserialize(reader);
+        if (classes == null)
+        {
+            throw new Exception();
+        }
+
+        return (T)classes;
+    }
+
+    /// <summary>
+    ///  Load classes from file info
+    /// </summary>
+    public static T From<T>(FileInfo info)
+    {
+        using var reader = info.OpenRead();
+
+        var serializer = XmlSerializer.FromTypes([typeof(T)])[0];
+        if (serializer == null)
+        {
+            throw new Exception();
+        }
+
+        var classes = serializer.Deserialize(reader);
+        if (classes == null)
+        {
+            throw new Exception();
+        }
+
+        return (T)classes;
+    }
 
     /// <summary>
     ///  Load fixml xml into classes from file path
     /// </summary>
     public static Xml.fix From(string xml)
-        => Fix.Load.From<Xml.fix>(xml);
+        => From<Xml.fix>(xml);
 }
