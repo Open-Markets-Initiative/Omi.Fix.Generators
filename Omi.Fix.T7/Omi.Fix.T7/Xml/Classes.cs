0cdb27c4a948f99b1846abde5e5f66ee7778d47b
Open-Markets-Initiative <contact.omi@protonmail.com>
2025-02-20 11:15:12 -0600

    Suppress complier warnings

--- Omi.Fix.T7/Omi.Fix.T7/Xml/Classes.cs
+++ Omi.Fix.T7/Omi.Fix.T7/Xml/Classes.cs
@@ -1,5 +1,7 @@
 ﻿namespace Omi.Fix.T7.Xml;
 
+#pragma warning disable CS8618
+
 [System.SerializableAttribute()]
 [System.ComponentModel.DesignerCategoryAttribute("code")]
 [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
